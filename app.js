be93ef0c979527810cd055ab8dd191ced94251aa
anhdungle <anhdungle634@gmail.com>
2020-06-26 08:30:12 +0200

    react to status switch

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 function hasNetwork(online) {
-  console.log(online);
+  const element = document.querySelector(".status");
+
+  if (online) {
+    element.classList.remove("offline");
+    element.classList.add("online");
+    element.innerText = "Online";
+  } else {
+    element.classList.remove("online");
+    element.classList.add("offline");
+    element.innerText = "Offline";
+  }
 }
 
 window.addEventListener("load", () => {
   hasNetwork(navigator.onLine);
+
+  window.addEventListener("online", () => {
+    hasNetwork(true);
+  });
+
+  window.addEventListener("offline", () => {
+    hasNetwork(false);
+  });
+
 });
